494f3ad40812568ed9d2ebce0e9dab7379ce42a8
Wen Yan <selinyan2016@gmail.com>
2024-01-26 13:41:05 +0200

    add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import "./app.scss";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="About me">Parallax</section>
+      <section id="Projects">Services</section>
+      <section>Project1</section>
+      <section>Project2</section>
+      <section>Project3</section>
+      <section id="Skills">Parallax</section>
+      <section id="Contact">Contact</section>
+    </div>
+  );
 };
 
 export default App;
