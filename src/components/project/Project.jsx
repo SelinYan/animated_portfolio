df453bbdeba3b009c0df8846c0095df5229cea7c
Wen Yan <selinyan2016@gmail.com>
2024-01-31 00:11:49 +0200

    add projects

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,16 +1,87 @@
+import { useRef } from "react";
 import "./project.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 const items = [
   {
     id: 1,
     title: "Zoo App",
     img: "project1.png",
-    desc: "",
+    desc: "Zoo App is designed to provide users with a seamless and engaging experience as they explore the diverse world of animals and birds",
+  },
+  {
+    id: 2,
+    title: "Chat-Chat App",
+    img: "project2.png",
+    desc: "Chat-Chat App is social networking application, which has diffierent features such as profile, timeline, friends, groups etc to build better connection for users",
+  },
+  {
+    id: 3,
+    title: "Pokedex App",
+    img: "project3.png",
+    desc: "Pokedex App enables users to explore a comprehensive database of Pokemon species, generations, their characteristics and abilities, by fetching Pokemon API data",
+  },
+  {
+    id: 4,
+    title: "Speed Game App",
+    img: "project4.png",
+    desc: "Speed Game App allowes users to input their names and choose different level to play the game, also has a conditional message based on their performance",
   },
 ];
 
+const Single = ({ item }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section ref={ref}>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imgContainer">
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <motion.button whileHover={{ backgroundColor: "#6f2dbd" }}>
+              See Demo
+            </motion.button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 const Project = () => {
-  return <div className="project">Project</div>;
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  return (
+    <div className="project" ref={ref}>
+      <div className="progress">
+        <h1>Latest Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Project;
